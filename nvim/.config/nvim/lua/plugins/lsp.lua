d5d8530f406ea634ff112107d50513fa1eb8747d
vshakitskiy <vshakitskiy@gmail.com>
2024-08-02 11:34:11 +0300

    nvim: rust lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,46 +1,68 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "tsserver",
-          "golangci_lint_ls",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+					"gopls",
+					"rust_analyzer",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local util = require("lspconfig.util")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.tsserver.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.golangci_lint_ls.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+				cmd = { "gopls" },
+				filetypes = { "go", "gomod", "gowork", "gotmpl" },
+				root_dir = function(fname)
+					-- see: https://github.com/neovim/nvim-lspconfig/issues/804
+					local mod_cache = vim.trim(vim.fn.system("go env GOMODCACHE"))
+					if fname:sub(1, #mod_cache) == mod_cache then
+						local clients = vim.lsp.get_active_clients({ name = "gopls" })
+						if #clients > 0 then
+							return clients[#clients].config.root_dir
+						end
+					end
+					return util.root_pattern("go.work")(fname) or util.root_pattern("go.mod", ".git")(fname)
+				end,
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+			})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>e", vim.diagnostic.goto_next, {
-        noremap = true,
-        silent = true,
-        buffer = bufnr,
-      })
-    end,
-  },
+			vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, {
+				noremap = true,
+				silent = true,
+				buffer = bufnr,
+			})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>e", vim.diagnostic.goto_next, {
+				noremap = true,
+				silent = true,
+				buffer = bufnr,
+			})
+		end,
+	},
 }
